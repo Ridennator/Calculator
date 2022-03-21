464de42cd00d5556e3fe58dc16a1c9db98ba6609
Ridennator <rlcpm@hotmail.com>
2022-03-21 17:11:17 +0000

    Visor updates according to input

--- Calculator/Calculator/Controllers/HomeController.cs
+++ Calculator/Calculator/Controllers/HomeController.cs
@@ -13,8 +13,50 @@ namespace Calculator.Controllers
             _logger = logger;
         }
 
+        [HttpGet] 
         public IActionResult Index()
         {
+            ViewBag.Visor = "0";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string botao, string visor)
+        {
+            // vamos decidir o q fazer com o valor do botao
+            switch (botao) {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+
+                    if(visor == "0") { visor = botao; }
+                    else { visor += botao; }
+
+                    break;
+                case ",":
+                    if (!visor.Contains(','))
+                    {
+                        visor += botao;
+                    }
+
+                    break;
+
+                case "+/-":
+                    if (visor.StartsWith('-')) { visor=visor[1..]; }
+                    else {visor = '-' + visor; }
+
+                    break;
+            }
+            ViewBag.Visor=visor;
+
+
             return View();
         }
 
